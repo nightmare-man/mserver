c548a04d497a20f20afa824906ee7faeed3f8b55
nightmare-man <107lsmlsm@whut.edu.cn>
2023-04-26 18:16:26 +0800

    user schema

---
+++ schemas/user.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, EmailStr
+
+from typing import Optional
+
+class UserBase(BaseModel):
+    email: Optional[EmailStr] = None
+    is_active: Optional[bool] = True
+    is_superuser: bool = False
+    full_name: Optional[str] = None
+
+class UserCreate(UserBase):
+    email: EmailStr
+    password: str
+
+class UserUpdate(UserBase):
+    password: Optional[str] = None
+
+class UserInDBBase(UserBase):
+    id: Optional[int] = None
+
+    class Config:
+        orm_mode = True
+
+class User(UserInDBBase):
+    pass
+
+class UserInDB(UserInDBBase):
+    hashed_password: str
