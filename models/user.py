7349db43903dceb5058399c0e5043c706452af57
nightmare-man <107lsmlsm@whut.edu.cn>
2023-04-26 10:02:42 +0800

    add user db model

--- models/user.py
+++ models/user.py
@@ -1,4 +1,13 @@
 from base import Base
-from sqlalchemy import 
+from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.orm import relationship
+
 class User(Base):
-    id:
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, index=True)
+    email =  Column(string,unique=True, index=True,nullable=False)
+    hashed_password= Column(String,nullable=False)
+    is_activate= Column(Boolean(),default=True)
+    is_superuser= Column(Boolean(),default=False)
+    items= relationship("Item",back_populates="owner")
+
