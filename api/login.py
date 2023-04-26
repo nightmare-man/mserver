efe83b1c33a52e6ac983f3d29060e27a871a745f
nightmare-man <107lsmlsm@whut.edu.cn>
2023-04-26 20:22:17 +0800

    添加登陆接口

---
+++ api/login.py
@@ -0,0 +1,9 @@
+from app import app
+from typing import Any
+
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy
+
+@app.post("/login")
+
--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-import uvicorn  
-from fastapi import FastAPI  
+import uvicorn 
+from typing import Any
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from api.deps import get_current_user
 app=FastAPI()
 @app.get("/healthy")
-async def healthy():
+async def healthy(user:Any = Depends(get_current_user)):
     return "I am ok!"
+@app.post("/login")
+async def login( form_data: OAuth2PasswordRequestForm = Depends()):
+    print(form_data.username)
+    print(form_data.password)
+
 if __name__=="__main__":
     uvicorn.run(app,host="0.0.0.0",port=9099)
